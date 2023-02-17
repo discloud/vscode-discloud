be197ab4e09c232ebceb0b0998612acea8a88b6a
Gorniaky <hwss.s2@gmail.com>
2023-02-16 22:20:05 -0300

    feat: Create Management of MODs of applications

---
+++ src/commands/apps/mods/rem.ts
@@ -0,0 +1,46 @@
+import { t } from "@vscode/l10n";
+import { RESTDeleteApiAppTeamResult, Routes } from "discloud.app";
+import { ProgressLocation, window } from "vscode";
+import { TaskData } from "../../../@types";
+import extension from "../../../extension";
+import AppTreeItem from "../../../structures/AppTreeItem";
+import Command from "../../../structures/Command";
+import { requester } from "../../../util";
+
+export default class extends Command {
+  constructor() {
+    super({
+      progress: {
+        location: ProgressLocation.Notification,
+        cancellable: true,
+        title: t("progress.mod.rem.title"),
+      },
+    });
+  }
+
+  async run(task: TaskData, item: AppTreeItem = <AppTreeItem>{}) {
+    if (!await this.confirmAction()) return;
+
+    if (!item.appId) {
+      item.appId = await this.pickApp(task);
+      if (!item.appId) return;
+    }
+
+    const modID = await window.showInputBox({
+      prompt: t("input.mod.rem.prompt"),
+    });
+    if (!modID) return;
+
+    const res = await requester<RESTDeleteApiAppTeamResult>(Routes.appTeam(item.appId, modID), {
+      method: "DELETE",
+    });
+
+    if ("status" in res) {
+      if (res.status === "ok") {
+        window.showInformationMessage(`${res.status}: ${res.message} - ID: ${item.appId}`);
+      } else {
+        window.showWarningMessage(`${res.status}: ${res?.message}`);
+      }
+    }
+  }
+}
