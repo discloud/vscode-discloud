61a59a168081875484d770b60c703a08caf1a50b
Gorniaky <hwss.s2@gmail.com>
2023-02-20 18:25:07 -0300

    feat: decline refresh command in case auto refresh is enabled

--- src/commands/subdomain/refresh.ts
+++ src/commands/subdomain/refresh.ts
@@ -1,3 +1,5 @@
+import { t } from "@vscode/l10n";
+import { window } from "vscode";
 import extension from "../../extension";
 import Command from "../../structures/Command";
 
@@ -7,6 +9,10 @@ export default class extends Command {
   }
 
   async run() {
-    await extension.appTree.fetch();
+    if (extension.autoRefresher.interval) {
+      window.showWarningMessage(t("command.declined.auto.refresh.is.activated"));
+    } else {
+      await extension.appTree.fetch();
+    }
   }
 }
