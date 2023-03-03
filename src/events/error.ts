874a54a2406815bdef56ab9e609caab650ef344d
Gorniaky <hwss.s2@gmail.com>
2023-03-03 08:30:09 -0300

    fix error logger

--- src/events/error.ts
+++ src/events/error.ts
@@ -2,6 +2,6 @@ import { window } from "vscode";
 import extension from "../extension";
 
 extension.on("error", (error) => {
-  extension.logger.error(error);
+  extension.logger.error(`${error.body?.message ?? error}`);
   window.showErrorMessage(`${error.body?.message ?? error}`);
 });
