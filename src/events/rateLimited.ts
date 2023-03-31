0915d20a1c5c005c2f3174d97b09189757bec629
Gorniaky <hwss.s2@gmail.com>
2023-03-31 17:46:51 -0300

    fix: rate limit count

--- src/events/rateLimited.ts
+++ src/events/rateLimited.ts
@@ -2,13 +2,21 @@ import { t } from "@vscode/l10n";
 import { window } from "vscode";
 import extension from "../extension";
 
+let ratelimits = 0;
+
 extension.on("rateLimited", (rateLimitData) => {
   extension.logger.warn("Rate limited by " + rateLimitData.time + " seconds");
+
+  ratelimits++;
 
   extension.statusBar.setRateLimited(true);
 
   setTimeout(() => {
-    extension.statusBar.setRateLimited(false);
+    ratelimits--;
+
+    if (!ratelimits) {
+      extension.statusBar.setRateLimited(false);
+    }
   }, rateLimitData.time * 1000);
 
   window.showInformationMessage(t("ratelimited", {
