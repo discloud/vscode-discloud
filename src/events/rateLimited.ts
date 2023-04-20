ff7f96f4a2865743992c9ceaf471043dcb27670d
Gorniaky <hwss.s2@gmail.com>
2023-04-20 08:00:29 -0300

    rateLimit enhancement

--- src/events/rateLimited.ts
+++ src/events/rateLimited.ts
@@ -2,24 +2,20 @@ import { t } from "@vscode/l10n";
 import { window } from "vscode";
 import extension from "../extension";
 
-let ratelimits = 0;
+let timer: NodeJS.Timer;
 
 extension.on("rateLimited", (rateLimitData) => {
+  clearTimeout(timer);
+
   extension.logger.warn("Rate limited by " + rateLimitData.time + " seconds");
-
-  ratelimits++;
-
-  extension.statusBar.setRateLimited(true);
-
-  setTimeout(() => {
-    ratelimits--;
-
-    if (!ratelimits) {
-      extension.statusBar.setRateLimited(false);
-    }
-  }, rateLimitData.time * 1000);
 
   window.showInformationMessage(t("ratelimited", {
     s: Math.floor(rateLimitData.time),
   }));
+
+  extension.statusBar.setRateLimited(true);
+
+  timer = setTimeout(() => {
+    extension.statusBar.setRateLimited(false);
+  }, rateLimitData.time * 1000);
 });
