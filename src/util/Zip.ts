b662462062728f3f66ea6358b1b8a2498ff10565
Gorniaky <hwss.s2@gmail.com>
2023-06-18 13:26:08 -0300

    fiz zipping files

--- src/util/Zip.ts
+++ src/util/Zip.ts
@@ -1,5 +1,5 @@
 import { Archiver, ArchiverOptions, create } from "archiver";
-import { createWriteStream, existsSync, rmSync, statSync, unlinkSync, writeFileSync, WriteStream } from "node:fs";
+import { WriteStream, createWriteStream, existsSync, rmSync, statSync, unlinkSync, writeFileSync } from "node:fs";
 
 export class Zip {
   declare stream: WriteStream;
@@ -17,20 +17,25 @@ export class Zip {
     this.zip.pipe(this.stream);
   }
 
-  appendFileList(fileList: string[], targetPath: string | RegExp, zipEmptyDirs?: boolean) {
+  appendFileList(fileList: string[], targetPath: string, zipEmptyDirs?: boolean) {
     if (!fileList?.length) return;
 
-    targetPath = RegExp(`^${targetPath}\\/?`, "i");
-
     for (const file of fileList) {
-      const name = file.replace(/\\/g, "/").replace(targetPath, "");
+      if (file.length === targetPath.length) continue;
+
+      let fileName = file;
+
+      if (file.length > targetPath.length)
+        fileName = file.slice(targetPath.length, fileName.length);
+
+      const name = fileName.replace(/\\/g, "/").replace(/^\//, "");
 
       if (!name) continue;
 
       if (existsSync(file))
         if (statSync(file).isFile()) {
           this.zip.file(file, { name });
-        } else if (zipEmptyDirs && statSync(file).isDirectory()) {
+        } else if (statSync(file).isDirectory() && zipEmptyDirs) {
           this.zip.file(file, { name });
         }
     }
