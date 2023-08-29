12a15df8ff838bcbb786e33dffa8ca67fc4448bc
Gorniaky <30408913+Gorniaky@users.noreply.github.com>
2023-08-28 23:09:48 -0300

    Feat/edit app profile avatar and name (#200)
    * feat: Edit app profile avatar and name
    
    * 2.16.0

---
+++ src/commands/apps/profile/avatar.ts
@@ -0,0 +1,49 @@
+import { t } from "@vscode/l10n";
+import { RESTApiBaseResult, Routes } from "discloud.app";
+import { window } from "vscode";
+import { BaseApiApp, TaskData } from "../../../@types";
+import extension from "../../../extension";
+import AppTreeItem from "../../../structures/AppTreeItem";
+import Command from "../../../structures/Command";
+import { requester } from "../../../util";
+
+export default class extends Command {
+  constructor() {
+    super();
+  }
+
+  async run(task: TaskData, item: AppTreeItem = <AppTreeItem>{}) {
+    if (!item.appId) {
+      const picked = await this.pickAppOrTeamApp(task, { showOther: false });
+      item.appId = picked.id;
+      if (!item.appId) throw Error(t("missing.appid"));
+    }
+
+    const avatarURL = await window.showInputBox({
+      prompt: t("input.avatar.prompt"),
+      validateInput(value) {
+        if (!/^(https?:\/\/.+\.(?:jpe?g|png))(?:\?.*)?$/.test(value))
+          return t("input.avatar.prompt");
+      },
+    });
+
+    if (!avatarURL) throw Error("Missing input");
+
+    if (!await this.confirmAction())
+      throw Error("Reject action");
+
+    const res = await requester<RESTApiBaseResult>(Routes.appProfile(item.appId), {
+      body: JSON.stringify({
+        avatarURL,
+      }),
+      method: "PUT",
+    });
+
+    if ("status" in res) {
+      this.showApiMessage(res);
+
+      if (res.status === "ok")
+        extension.appTree.edit(item.appId, <BaseApiApp>{ id: item.appId, avatarURL });
+    }
+  }
+}
