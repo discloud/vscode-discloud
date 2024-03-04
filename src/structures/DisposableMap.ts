289005da1f9b0d162fc949df1204507b17e82fdb
Gorniaky <hwss.s2@gmail.com>
2024-03-04 16:58:36 -0300

    fix disposable error

--- src/structures/DisposableMap.ts
+++ src/structures/DisposableMap.ts
@@ -6,14 +6,14 @@ export default class DisposableMap<K, V extends Disposable> extends Map<K, V> {
   dispose(key?: K) {
     if (key === undefined) {
       for (const disposable of this.values()) {
-        disposable.dispose();
+        disposable.dispose?.();
       }
       this.clear();
     } else {
       const existing = this.get(key);
 
       if (existing) {
-        existing.dispose();
+        existing.dispose?.();
         return this.delete(key);
       }
 
