e8e24db0dd5864e1ef808371069c8cd91e5f92c2
Gorniaky <hwss.s2@gmail.com>
2025-02-08 15:04:09 -0300

    .

--- src/events/missingToken.ts
+++ src/events/missingToken.ts
@@ -1,8 +1,9 @@
+import { t } from "@vscode/l10n";
 import extension from "../extension";
 
 extension.on("missingToken", async function () {
   extension.userTree.clear();
   extension.statusBar.setLogin();
 
-  extension.logger.warn("Missing token");
+  extension.logger.warn(t("missing.token"));
 });
