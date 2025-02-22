beb3ea72dbfb6bd796c0ed4bec3f050446a7f0ba
Gorniaky <hwss.s2@gmail.com>
2025-02-22 16:59:05 -0300

    perf: using context to get l10n path in packageJSON

--- src/localize.ts
+++ src/localize.ts
@@ -5,7 +5,7 @@ import { env, type ExtensionContext } from "vscode";
 
 function importJSON<T extends l10nJsonFormat>(path: string): T {
   try {
-    if (existsSync(path)) 
+    if (existsSync(path))
       return JSON.parse(readFileSync(path, "utf8"));
 
     return <T>{};
@@ -14,17 +14,20 @@ function importJSON<T extends l10nJsonFormat>(path: string): T {
   }
 }
 
-const bundleDir = "l10n";
-
 export function localize(context: ExtensionContext) {
+  const firstLanguagePart = env.language.split(/\W+/)[0];
+  const bundleDir: string = context.extension.packageJSON.l10n;
+
   config({
     contents: Object.assign({},
       importJSON(context.asAbsolutePath("package.nls.json")),
-      importJSON(context.asAbsolutePath(`package.nls.${env.language.split(/\W+/)[0]}.json`)),
+      importJSON(context.asAbsolutePath(`package.nls.${firstLanguagePart}.json`)),
       importJSON(context.asAbsolutePath(`package.nls.${env.language}.json`)),
-      importJSON(context.asAbsolutePath(join(bundleDir, "bundle.l10n.json"))),
-      importJSON(context.asAbsolutePath(join(bundleDir, `bundle.l10n.${env.language.split(/\W+/)[0]}.json`))),
-      importJSON(context.asAbsolutePath(join(bundleDir, `bundle.l10n.${env.language}.json`))),
+      ...bundleDir ? [
+        importJSON(context.asAbsolutePath(join(bundleDir, "bundle.l10n.json"))),
+        importJSON(context.asAbsolutePath(join(bundleDir, `bundle.l10n.${firstLanguagePart}.json`))),
+        importJSON(context.asAbsolutePath(join(bundleDir, `bundle.l10n.${env.language}.json`))),
+      ] : [],
     ),
   });
 }
