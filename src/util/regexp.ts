82d4611c65b49eeaa8be8a8fa75ee08d5e1d3f81
Gorniaky <hwss.s2@gmail.com>
2025-02-23 14:17:45 -0300

    fix regexp

--- src/util/regexp.ts
+++ src/util/regexp.ts
@@ -1,6 +1,6 @@
 import { NODE_MODULES_EXTENSIONS } from "./constants";
 
 export const NODE_MODULES_EXTENSIONS_REGEXP =
-  RegExp(`${Array.from(NODE_MODULES_EXTENSIONS).join("|").replace(/(\.)/g, "\\$1")}$`, "i");
+  RegExp(`(${Array.from(NODE_MODULES_EXTENSIONS).join("|").replace(/(\.)/g, "\\$1")})$`, "i");
 
 export const IMAGE_URL_REGEXP = /^((?:s?ftp|https?):\/\/\S+\.(?:gif|jpe?g|png))(?:[?]\S*)?$/;
