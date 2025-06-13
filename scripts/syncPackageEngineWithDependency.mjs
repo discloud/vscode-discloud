4f45f93c98af35223cd5bb8ab3f48bc308252d32
Gorniaky <hwss.s2@gmail.com>
2025-06-12 22:26:02 -0300

    feat: implement sync engine version workflow and update script functionality

--- scripts/syncPackageEngineWithDependency.mjs
+++ scripts/syncPackageEngineWithDependency.mjs
@@ -1,4 +1,4 @@
-import { readFile } from "fs/promises";
+import { readFile, writeFile } from "fs/promises";
 
 const PACKAGE_PATH = "package.json";
 
@@ -10,19 +10,30 @@ const encodedPackage = await readFile(PACKAGE_PATH, "utf8");
 
 const packageJSON = JSON.parse(encodedPackage);
 
-if (!packageJSON.engines[engine]) throw new Error(`Engine '${engine}' not found on '${PACKAGE_PATH}'`);
+const oldEngineVersion = packageJSON.engines?.[engine];
+
+if (oldEngineVersion === undefined) throw new Error(`Engine '${engine}' not found on '${PACKAGE_PATH}'`);
 
 const packageJSONDependecyKeyRegexp = /^(dependencies|\w+Dependencies)$/;
 
-let found = false;
+let found = false, updated = false;
 for (const key in packageJSON) {
   if (!packageJSONDependecyKeyRegexp.test(key)) continue;
   if (!(dependency in packageJSON[key])) continue;
-  packageJSON.engines[engine] = packageJSON[key][dependency];
+  if (oldEngineVersion !== packageJSON[key][dependency]) {
+    packageJSON.engines[engine] = packageJSON[key][dependency];
+    updated = true;
+  }
   found = true;
   break;
 }
 
 if (!found) throw new Error(`Dependency '${dependency}' not found on '${PACKAGE_PATH}'`);
 
-console.log(`Engine ${engine} has been updated`);
+if (updated) {
+  await writeFile(PACKAGE_PATH, JSON.stringify(packageJSON, null, 2), "utf8");
+
+  console.log(`Engine '${engine}' has been updated to '${packageJSON.engines[engine]}'`);
+} else {
+  console.log(`Engine '${engine}' is already updated`);
+}
