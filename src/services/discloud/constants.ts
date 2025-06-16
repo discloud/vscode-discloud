76a629e35d4ba5bac28a117084dfe5a2bfdcfadf
Gorniaky <hwss.s2@gmail.com>
2025-06-16 10:26:28 -0300

    fix: correct MAX_ZIP_BUFFER_PART value to reflect accurate size

--- src/services/discloud/constants.ts
+++ src/services/discloud/constants.ts
@@ -1,8 +1,8 @@
 /** `100MB` */
 export const MAX_ZIP_BUFFER = 104_857_600;
 
-/** `99MB` */
-export const MAX_ZIP_BUFFER_PART = 103_809_024;
+/** `50MB` */
+export const MAX_ZIP_BUFFER_PART = 52_428_800;
 
 /** `512MB` */
 export const MAX_UPLOAD_SIZE = 536_870_912;
