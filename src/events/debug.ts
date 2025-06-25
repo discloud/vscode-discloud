728a42a2545f7ccfcbd57223e5bd6cccc97719f3
Gorniaky <hwss.s2@gmail.com>
2025-06-24 23:18:01 -0300

    fix: format debug message logging for consistency

--- src/events/debug.ts
+++ src/events/debug.ts
@@ -1,6 +1,7 @@
+import { format } from "util";
 import extension from "../extension";
 
 extension.on("debug", async function (message, ...args) {
-  if (extension.isDebug) extension.logger.info(message, ...args);
-  else extension.logger.debug(message, ...args);
+  if (extension.isDebug) extension.logger.info(format(message, ...args));
+  else extension.logger.debug(format(message, ...args));
 });
