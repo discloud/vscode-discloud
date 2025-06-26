0a1ed3281a85a215381aeb4e791bf48715fe535f
Gorniaky <hwss.s2@gmail.com>
2025-06-26 20:20:51 -0300

    fix: update image URL regex pattern for consistency and remove unnecessary protocols

--- src/util/regexp.ts
+++ src/util/regexp.ts
@@ -1,1 +1,1 @@
-export const IMAGE_URL_REGEXP = /^((?:s?ftp|https?):\/\/\S+\.(?:gif|jpe?g|png|webp))(?:[?#]\S*)?$/;
+export const IMAGE_URL_REGEXP = /^(https?:\/\/\S+\.(?:gif|jpe?g|png|webp))(?:[?#]\S*)?$/;
