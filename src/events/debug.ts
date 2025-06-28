3ea314b699360c879fa35692eb53a6b10b4ad5be
Gorniaky <hwss.s2@gmail.com>
2025-06-28 17:45:54 -0300

    fix: update logging method in debug event handler

--- src/events/debug.ts
+++ src/events/debug.ts
@@ -2,6 +2,6 @@ import { format } from "util";
 import extension from "../extension";
 
 extension.on("debug", async function (message, ...args) {
-  if (extension.isDebug) extension.logger.info(format(message, ...args));
+  if (extension.isDebug) extension.logger.appendLine(format(message, ...args));
   else extension.logger.debug(format(message, ...args));
 });
