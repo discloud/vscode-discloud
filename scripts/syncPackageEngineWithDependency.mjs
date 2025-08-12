86f9ab3fe1cee18307dfaa6ad7a0ebb1be11c99c
Gorniaky <hwss.s2@gmail.com>
2025-08-12 10:27:05 -0300

    refactor(scripts): improve syncPackageEngineWithDependency script
    Introduced a constant for the file encoding, removing the "utf8" magic string and improving code readability.

--- scripts/syncPackageEngineWithDependency.mjs
+++ scripts/syncPackageEngineWithDependency.mjs
@@ -1,5 +1,6 @@
 import { readFile, writeFile } from "fs/promises";
 
+const ENCODING = "utf8";
 const INDENTATION = 2;
 const PACKAGE_PATH = "package.json";
 
@@ -7,7 +8,7 @@ const [, , engine, dependency] = process.argv;
 
 if (!engine || !dependency) throw new Error("Use: node scripts/syncPackageEngineWithDependency.mjs <engineName> <dependencyName>");
 
-const encodedPackage = await readFile(PACKAGE_PATH, "utf8");
+const encodedPackage = await readFile(PACKAGE_PATH, ENCODING);
 
 /** @type {import("type-fest").PackageJson} */
 const packageJSON = JSON.parse(encodedPackage);
@@ -32,7 +33,9 @@ for (const key in packageJSON) {
 if (!found) throw new Error(`Dependency '${dependency}' not found on '${PACKAGE_PATH}'`);
 
 if (updated) {
-  await writeFile(PACKAGE_PATH, JSON.stringify(packageJSON, null, INDENTATION), "utf8");
+  const content = JSON.stringify(packageJSON, null, INDENTATION);
+
+  await writeFile(PACKAGE_PATH, content, ENCODING);
 
   console.log(`Engine '${engine}' has been updated to '${packageJSON.engines[engine]}'`);
 } else {
