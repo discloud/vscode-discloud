d54239fb0e7807adc1b2b94360822af6abcfb14d
Gorniaky <hwss.s2@gmail.com>
2026-06-09 16:19:04 -0300

    bump deps

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,4 +1,5 @@
 import { strictEqual } from "assert";
+import { suite, test } from "node:test";
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
