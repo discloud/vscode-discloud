ab71d5c668d3f67348f5628d588cbf7f69c9d846
Gorniaky <hwss.s2@gmail.com>
2026-09-18 14:28:05 -0300

    chore(discloud): update MAX_FILE_SIZE from `512MB` to `1GB`

--- src/services/discloud/constants.ts
+++ src/services/discloud/constants.ts
@@ -7,13 +7,13 @@ export const DEFAULT_CHUNK_SIZE = 262_144;
 /** `1MB` */
 export const MAX_CHUNK_SIZE = 1_048_576;
 
-/** `512MB` */
-export const MAX_FILE_SIZE = 536_870_912;
+/** `1GB` */
+export const MAX_FILE_SIZE = 1_073_741_824;
 
-export const NETWORK_UNREACHABLE_ERRNO = -3008 as const;
+export const NETWORK_UNREACHABLE_ERRNO = -3008;
 
-export const NETWORK_UNREACHABLE_CODE = "ENOTFOUND" as const;
+export const NETWORK_UNREACHABLE_CODE = "ENOTFOUND";
 
-export const SOCKET_ABNORMAL_CLOSURE = 1006 as const;
+export const SOCKET_ABNORMAL_CLOSURE = 1006;
 
-export const SOCKET_UNAUTHORIZED_CODE = 3000 as const;
+export const SOCKET_UNAUTHORIZED_CODE = 3000;
